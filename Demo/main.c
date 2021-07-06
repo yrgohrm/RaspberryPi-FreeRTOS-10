114b194c1811518a736501f66f408dda6b5bdc3d
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-07-06 13:43:53 +0100

    Remove some tabs

--- Demo/main.c
+++ Demo/main.c
@@ -7,59 +7,59 @@
 static volatile int blink = 1;
 
 void task1(void *pParam) {
-	int i = 0;
-	while(1) {
-		i++;
-		if (blink)
-			SetGpio(16, 1);
-		vTaskDelay(200);
-	}
+    int i = 0;
+    while(1) {
+        i++;
+        if (blink)
+            SetGpio(16, 1);
+        vTaskDelay(200);
+    }
 }
 
 void task2(void *pParam) {
-	int i = 0;
-	while(1) {
-		i++;
-		vTaskDelay(100);
-		SetGpio(16, 0);
-		vTaskDelay(100);
-	}
+    int i = 0;
+    while(1) {
+        i++;
+        vTaskDelay(100);
+        SetGpio(16, 0);
+        vTaskDelay(100);
+    }
 }
 
 void button_handler(unsigned int irq, void *pParam) {
-	if (EventDetected(12)) {
-		blink = !blink;
-		ClearGpioInterrupt(12);
-	}
+    if (EventDetected(12)) {
+        blink = !blink;
+        ClearGpioInterrupt(12);
+    }
 }
 
 /**
- *	This is the systems main entry, some call it a boot thread.
+ *    This is the systems main entry, some call it a boot thread.
  *
- *	-- Absolutely nothing wrong with this being called main(), just it doesn't have
- *	-- the same prototype as you'd see in a linux program.
+ *    -- Absolutely nothing wrong with this being called main(), just it doesn't have
+ *    -- the same prototype as you'd see in a linux program.
  **/
 void main (void)
 {
-	SetGpioFunction(16, GPIO_OUT);		// LED on GPIO 16
-	SetGpioFunction(12, GPIO_IN);		// Button on GPIO 12
+    SetGpioFunction(16, GPIO_OUT);        // LED on GPIO 16
+    SetGpioFunction(12, GPIO_IN);        // Button on GPIO 12
 
-	EnableGpioDetect(12, DETECT_RISING);
-	ClearGpioInterrupt(12);
+    EnableGpioDetect(12, DETECT_RISING);
+    ClearGpioInterrupt(12);
 
-	irqRegister(49, button_handler, NULL);
-	irqEnable(49);
+    irqRegister(49, button_handler, NULL);
+    irqEnable(49);
 
-	xTaskCreate(task1, (const signed char*)"LED_0", 128, NULL, 0, NULL);
-	xTaskCreate(task2, (const signed char*)"LED_1", 128, NULL, 0, NULL);
+    xTaskCreate(task1, (const signed char*)"LED_0", 128, NULL, 0, NULL);
+    xTaskCreate(task2, (const signed char*)"LED_1", 128, NULL, 0, NULL);
 
-	vTaskStartScheduler();
+    vTaskStartScheduler();
 
-	/*
-	 *	We should never get here, but just in case something goes wrong,
-	 *	we'll place the CPU into a safe loop.
-	 */
-	while(1) {
-		;
-	}
+    /*
+     *    We should never get here, but just in case something goes wrong,
+     *    we'll place the CPU into a safe loop.
+     */
+    while(1) {
+        ;
+    }
 }
