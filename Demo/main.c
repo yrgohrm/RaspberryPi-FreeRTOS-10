363e9c3397f3b436543ada43ce093ec4a55253ed
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-07-07 14:34:06 +0200

    Gave it some love and formatting

--- Demo/main.c
+++ Demo/main.c
@@ -6,26 +6,31 @@
 
 static volatile int blink = 1;
 
-void task1(void *pParam) {
-    while(1) {
-        if (blink)
+void task1(void *pParam __attribute__((unused)))
+{
+    while (1) {
+        if (blink) {
             SetGpio(16, 1);
-        vTaskDelay(200);
+        }	  
+	vTaskDelay(200);
     }
 }
 
-void task2(void *pParam) {
-    while(1) {
-        vTaskDelay(100);
-        SetGpio(16, 0);
-        vTaskDelay(100);
+void task2(void *pParam __attribute__((unused)))
+{
+    while (1) {
+	vTaskDelay(100);
+	SetGpio(16, 0);
+	vTaskDelay(100);
     }
 }
 
-void button_handler(unsigned int irq, void *pParam) {
+void button_handler(unsigned int irq __attribute__((unused)),
+		    void *pParam __attribute__((unused)))
+{
     if (EventDetected(12)) {
-        blink = !blink;
-        ClearGpioInterrupt(12);
+	blink = !blink;
+	ClearGpioInterrupt(12);
     }
 }
 
@@ -35,11 +40,14 @@ void button_handler(unsigned int irq, void *pParam) {
  *    -- Absolutely nothing wrong with this being called main(), just it doesn't have
  *    -- the same prototype as you'd see in a linux program.
  **/
-void main (void)
+void main(void)
 {
-    SetGpioFunction(16, GPIO_OUT);        // LED on GPIO 16
-    SetGpioFunction(12, GPIO_IN);        // Button on GPIO 12
+    // Initialize GPIO pins
+    SetGpioFunction(16, GPIO_OUT);	// LED on GPIO 16
+    SetGpioFunction(12, GPIO_IN);	// Button on GPIO 12
 
+    // Set up GPIO interrupt on rising edge for GPIO 12
+    
     irqBlock();
 
     EnableGpioDetect(12, DETECT_RISING);
@@ -50,16 +58,21 @@ void main (void)
 
     irqUnblock();
 
-    xTaskCreate(task1, (const signed char*)"LED_0", 128, NULL, 0, NULL);
-    xTaskCreate(task2, (const signed char*)"LED_1", 128, NULL, 0, NULL);
 
+    // Create the two tasks, both with priority 0
+    
+    xTaskCreate(task1, "LED_ON",  128, NULL, 0, NULL);
+    xTaskCreate(task2, "LED_OFF", 128, NULL, 0, NULL);
+
+    // Get things going by starting up the scheduler
+    
     vTaskStartScheduler();
 
     /*
      *    We should never get here, but just in case something goes wrong,
      *    we'll place the CPU into a safe loop.
      */
-    while(1) {
-        ;
+    while (1) {
+	;
     }
 }
